f78dae6451ba3942eb281f64864cd30ffc6748be
Ed Chadwick <edwardmchadwick@gmail.com>
2023-05-16 10:06:51 +0100

    File asset field finally working

---
+++ resources/js/Pages/Homepage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import UserWelcome from './UserWelcome'
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: Object })
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello {{ user.name }}, Welcome to Afterchef!</p>
+</template>
